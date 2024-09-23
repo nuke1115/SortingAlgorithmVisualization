d907d6aaba53a6604c94d14a19c666687382bdcf
unta1337 <firstapple42@gmail.com>
2024-09-23 20:09:30 +0900

    Correct backslash for escaping

--- sourceCode/SortingAlgorithmVisualization/header/Implements/InputManager.cpp
+++ sourceCode/SortingAlgorithmVisualization/header/Implements/InputManager.cpp
@@ -40,7 +40,7 @@ void InputManager::GetCommand(Command& command,volatile std::atomic<bool>& runni
             }
             else if (!command.command.compare("Sort"))
             {
-                std::cout << "Enter Sorting Algorithm's name which you want to execute/n";
+                std::cout << "Enter Sorting Algorithm's name which you want to execute\n";
 
                 std::cin >> command.strArg;
             }
